64285f7cc8fb4eeaaa537d4113cf880f56a3ec15
unknown <thuantt01011992@gmail.com>
2024-04-02 15:13:39 +0700

    quickSort Algorithm - Asc adn Desc

--- pydsandalgorithms/pyQuickSort.py
+++ pydsandalgorithms/pyQuickSort.py
@@ -29,7 +29,7 @@
 # Chọn 70 làm phần tử chốt 
 #                           [10, 80, 30, 90, 40, 50, 70]
 #  Partition  70                 ||                  ||                      
-#                           [10, 30, 40, 50]        [90, 80] Partion arround 80
+#                           [10, 30, 40, 50]        [90, 80] Partion arround 80  ==> []
 #  Partition 50                 ||                    || 
 #                       [10, 30 ,40 ]                [90] 
 #  Partition 40                ||
@@ -47,6 +47,112 @@
 # 6. Khi biến bên phải nhỏ hơn phần tử chốt thì di chuyển sang trái
 # 7. Nếu không xảy ra trường hợp 5 6 thì hoán đổi giá trị 2 biến trái phải
 # 8. Nếu trái lớn hơn phải thì đây là giá trị chốt mới.
+
+# https://www.youtube.com/watch?v=r637qlJvtlk
+# The algorithm finished when all the numbers have been considered as pivots.
+
+# 13, 21, 15, 3, |12|, 9, 14, 7, 6
+# 3, 9, |7|, 6, 12, 13, 21, |15|, 14 
+# 3, |6|, 7, |9|, 12, 13, |14|, 15, |21|
+# |3|, 6, 7, 9, 12, |13|, 14, 15, 21
+# 3, 6, 7, 9, 12, 13, 14, 15, 21
+
+# 1. Chọn phần tử chốt ( pivot )
+# 2. khai báo 2 biến con trỏ để duyệt 2 phía của phần tử chôt
+# 3. Biến bên trái trỏ đến từng phần tử mảng con bên trái của phần tử chốt
+# 4. Biến bên phải trỏ đến từng phần tử mảng con bên phải của phần tử chốt
+# 5. Cho đến khi biến bên trái nhỏ hơn phần tử chốt thì nhảy sang bên phải
+# 6. Cho đến khi biến bên phải nhỏ hơn phần tử chốt thì nhảy sang trái
+# 7. Nếu 5,6 không xảy ra thì swap 2 giá trị trái phải
+# 8. Nếu trái lớn hơn phải thì đây là giá trị chốt mới.
+
+
+# Sắp xếp giảm dần
+def quickSortDesc(numbers, left, right):
+
+    if numbers is None:
+        return
+    
+    # Duyệt đến khi còn 1 phần tử left = right
+    if left >= right:
+        return
+    
+    # Chọn phần tử chốt là phần tử đứng giữa từ left đến right    
+    middle =   int( left + (right - left)/2)
+    pivot = numbers[middle]
+
+    # Khai báo con trỏ bên trái và bên phải 
+    i, j = left, right
+
+    # Tiếp tục duyệt cho đến khi index trái = index phải
+    while( i <= j ):
+
+        # duyệt vào mảng con bên trái, nếu giá trị của phần tử được duyệt > giá trị pivot thì break ra để tiến hành swap sang bên phải
+        while (numbers[i] > pivot):
+            i = i + 1
+
+        # duyệt vào mảng con bên phải, nếu giá trị phần tử được duyệt < pivot thì break để tiến hành swap sang trái
+        while (numbers[j] < pivot):
+            j = j - 1
+
+        # tiến hành swap
+        if i <= j:
+            temp = numbers[i]
+            numbers[i] = numbers[j]
+            numbers[j] = temp
+
+            # duyệt đến phần tử tiếp theo bên trái
+            i = i + 1
+
+            # duyệt đến phần tử tiếp theo bên phải
+            j = j - 1
+
+    # Tiếp tục sắp xếp mảng bên trái từ vị trí left đến vị trí j mới            
+    if left < j : 
+        quickSortDesc(numbers, left, j)
+    
+    # Tiếp tục sắp xếp mảng bên phải từ vị trí i mới đến vị trí right
+    if right > i:
+        quickSortDesc(numbers, i, right)
+                  
+# pivot is middle of list
+def quickSortAsc(numbers, left, right):
+    
+    if numbers is None:
+        return
+    
+    if left >= right:
+        return
+    
+    middle = int(left + ( right - left ) / 2 )        
+
+    pivot = arr[middle]
+    i, j = left, right
+    
+    while (i <= j) :
+        
+        print(f"before first loop : (i, j) = ({i}, {j}), pivot = {pivot}")            
+
+        while (arr[i] < pivot):
+             i = i + 1
+        
+        while( arr[j] > pivot ):
+            j = j - 1
+            
+        if ( i <= j ):
+            temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+            i = i + 1
+            j = j - 1
+                
+    if left < j:
+        print(f"( left, j ) = ({left},{j})")
+        quickSortAsc(arr, left, j)
+
+    if right > i:
+        print(f"( i, right ) = ({i},{right})")
+        quickSortAsc(arr, i, right)
 
 def max(list):
     result = None
@@ -74,59 +180,68 @@ def min(list):
 
     return result
 
-def quickSort(numbers, left, right):
-    
-    if arr is None:
+def quickSort(numbers, left, right, typeOrder = None):
+
+    # Nếu danh sách null thì ta return
+    if numbers is None:
         return
-    
+
+    # Dừng lại nếu danh sách chỉ còn 1 phần tử
     if left >= right:
         return
     
-    middle = int(left + ( right - left ) / 2 )        
-
-    pivot = arr[middle]
+    # Chọn phần tử chốt ( pivot )
+    middle = int( left + (right - left)/ 2)
+    pivot = numbers[middle]
     i, j = left, right
-    
-    while (i <= j) :
 
-        while (arr[i] < pivot):
-             i = i + 1
+    # Partition
+    while ( i <= j):
+
+
+        if typeOrder is None:
+
+            # Duyệt vào mảng bên trái partition        
+            while (numbers[i] < pivot ) :
+                i = i + 1
+            
+            # Duyệt vào phần tử mảng con bên phải partition
+            while numbers[j] > pivot:
+                j = j - 1
+        elif typeOrder == 'Desc':
+
+            while numbers[i] > pivot:
+                i += 1
+
+            while numbers[j] < pivot:
+                j += -1 
         
-        while( arr[j] > pivot ):
-            j = j - 1
+        # Tiến hành swap
+        if i <= j:
+            temp = numbers[i]
+            numbers[i] = numbers[j]
+            numbers[j] = temp
 
-        if ( i <= j ):
-            temp = arr[i]
-            arr[i] = arr[j]
-            arr[j] = temp
-            i = i + 1
-            j = j - 1        
-    
-    print('-----------------------------------')
-    print(f"(i,j) = ( {i},{j} ) , left,right  = ( {left},{right} )")
-    print(arr)
-        
-    if left < j:
-        print(f"( left, j ) = ({left},{j})")
-        quickSort(arr, left, j)
-
-    if right > i:
-        print(f"( i, right ) = ({i},{right})")
-        quickSort(arr, i, right)
+            # duyệt vào các phần tử tiếp theo
+            i += 1
+            j += -1        
+            
 
 if __name__=="__main__":
     
-    arr = [6, 2, 3, 4, 5, 9, 1]   
-
+    arr = [3, 6, 7, 9, 12, 13, 14, 15, 21]      
     left = 0
     right = len(arr) - 1
-
-    quickSort(arr, left, right)
-    print('--------------------')
+        
+    quickSort(arr, left, right)    
     print(arr)
 
-
-
+    arr = [3, 6, 7, 9, 12, 13, 14, 15, 21]      
+    left = 0
+    right = len(arr) - 1
+        
+    quickSort(arr, left, right, 'Desc')    
+    print(arr)
 
 
 
